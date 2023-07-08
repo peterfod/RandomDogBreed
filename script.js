525aa90554c77a938507a9cf3d59c3308d548504
peter <peterfod@freemail.hu>
2023-07-08 21:22:44 +0200

    add new files

---
+++ script.js
@@ -0,0 +1,35 @@
+const imgDog = document.querySelector('#img-dog')
+const btnDog = document.querySelector('#btn-dog')
+const breedSelect = document.querySelector('#breed')
+const breedsURL = 'https://dog.ceo/api/breeds/list/all'
+
+window.addEventListener('load', getBreeds)
+btnDog.addEventListener('click', getDogImage)
+breedSelect.addEventListener('change', getDogImage)
+
+async function getBreeds() {
+  try {
+    const response = await fetch(breedsURL)
+    const data = await response.json()
+    Object.keys(data.message).forEach(function (key) {
+      let option = document.createElement('option')
+      option.value = key
+      option.innerHTML = key
+      breedSelect.appendChild(option)
+    })
+  } catch (error) {
+    imgDog.innerHTML = '<p>Az API nem érhető el!<br>' + error + '</p>'
+  }
+}
+
+async function getDogImage() {
+  try {
+    const API_URL = `https://dog.ceo/api/breed/${breedSelect.value}/images/random`
+    const response = await fetch(API_URL)
+    const data = await response.json()
+    imgDog.innerHTML = `<img src="${data.message}">`
+    console.log(breedSelect.value)
+  } catch (error) {
+    imgDog.innerHTML = '<p>Az API nem érhető el!<br>' + error + '</p>'
+  }
+}
